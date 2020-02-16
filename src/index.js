d72242c8ea2541f3de836ee8656d77c460f3c454
Rob Howell <rob@robhowell.com>
2020-02-16 19:27:31 +0000

    Add console debug to check number of commits

--- src/index.js
+++ src/index.js
@@ -2,32 +2,36 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const validatePrTitle = require('./validatePrTitle');
 
-module.exports = async function run() {
+async function run() {
   try {
     const client = new github.GitHub(process.env.GITHUB_TOKEN);
 
-    const contextPullRequest = github.context.payload.pull_request;
-    if (!contextPullRequest) {
+    const pullRequestContext = github.context.payload.pull_request;
+    if (!pullRequestContext) {
       throw new Error(
         "This action can only be invoked in `pull_request` events. Otherwise the pull request can't be inferred."
       );
     }
 
-    const owner = contextPullRequest.base.user.login;
-    const repo = contextPullRequest.base.repo.name;
+    const owner = pullRequestContext.base.user.login;
+    const repo = pullRequestContext.base.repo.name;
 
     // The pull request info on the context isn't up to date. When
     // the user updates the title and re-runs the workflow, it would
     // be outdated. Therefore fetch the pull request via the REST API
     // to ensure we use the current title.
-    const {data: pullRequest} = await client.pulls.get({
+    const {data: pullRequestData} = await client.pulls.get({
       owner,
       repo,
-      pull_number: contextPullRequest.number
+      pull_number: pullRequestContext.number
     });
 
-    await validatePrTitle(pullRequest.title);
+    console.log(`Number of commits = ${!!pullRequest.commits && pullRequest.commits.length}`);
+
+    await validatePrTitle(pullRequestData.title);
   } catch (error) {
     core.setFailed(error.message);
   }
 };
+
+module.exports = run;
